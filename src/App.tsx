7f85f2d8019c2d925f40b219ca20a5a7096fc578
jakewhiteyo <jakewhiteyo@gmail.com>
2022-10-26 17:22:44 -0600

    got router working

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,58 @@ import './App.css';
 import { useState } from 'react';
 import LoginPage from './pages/login/login';
 import HomePage from './pages/homePage/homePage';
-import { AppShell, Navbar, Header, Burger, Anchor } from '@mantine/core';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Burger,
+  Anchor,
+  MantineProvider,
+} from '@mantine/core';
+import { Route, Routes } from 'react-router-dom';
+import DefaultHome from './pages/defaultHome';
 
 function App() {
   const [opened, setOpened] = useState(false);
   return (
-    <AppShell
-      fixed
-      header={
-        <Header height={50}>
-          <Burger
-            opened={opened}
-            onClick={() => setOpened((o) => !o)}
-            size="lg"
-            mr="lg"
-            ml="lg"
-          />
-        </Header>
-      }
-      navbar={
-        <Navbar hiddenBreakpoint="xl" width={{ base: '100%' }} hidden={!opened}>
-          <Anchor>Home</Anchor>
-          <Anchor>Features</Anchor>
-          <Anchor>Pricing</Anchor>
-        </Navbar>
-      }
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'dark',
+      }}
     >
-      App
-    </AppShell>
+      <AppShell
+        fixed
+        header={
+          <Header height={50}>
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              size="lg"
+              mr="lg"
+              ml="lg"
+            />
+          </Header>
+        }
+        navbar={
+          <Navbar
+            hiddenBreakpoint="xl"
+            width={{ base: '100%' }}
+            hidden={!opened}
+          >
+            <Anchor>Home</Anchor>
+            <Anchor>Default Home</Anchor>
+            <Anchor>Features</Anchor>
+            <Anchor>Pricing</Anchor>
+          </Navbar>
+        }
+      >
+        <Routes>
+          <Route path="/default" element={<DefaultHome />} />
+        </Routes>
+      </AppShell>
+    </MantineProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { MantineProvider } from '@mantine/core';
 import './mantineComponenetStyling/mantineProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'dark',
-      }}
-    >
+  <StrictMode>
+    <Router>
       <App />
-    </MantineProvider>
-  </React.StrictMode>
+    </Router>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
