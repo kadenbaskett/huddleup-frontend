3001a0308c7f8d37025e308299085bf7fa11184e
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2022-10-26 15:03:42 -0600

    Linter working

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
 import LoginPage from './pages/loginPage/loginPage';
-import HomePage from './pages/homePage/homePage'
-import { AppShell, Navbar, Header, MediaQuery, Burger, Anchor} from "@mantine/core";
+import HomePage from './pages/homePage/homePage';
+import { AppShell, Navbar, Header, Burger, Anchor } from '@mantine/core';
 
 function App() {
   const [opened, setOpened] = useState(false);
   return (
     <AppShell
-        fixed
-        // navbarOffsetBreakpoint="sm"
-        header={
-          <Header height={50}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="lg"
-                mr="lg"
-                ml="lg"
-              />
-          </Header>
-        }
-        navbar={
-          <Navbar
-            // className={classes.navbar}
-            width={{ base: "100%"}}
-            hidden={!opened}
-          >
-            <Anchor>Home</Anchor>
-            <Anchor>Features</Anchor>
-            <Anchor>Pricing</Anchor>
-          </Navbar>
-        }
-      >
-        App
-      </AppShell>
+      fixed
+      // navbarOffsetBreakpoint="sm"
+      header={
+        <Header height={50}>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            size="lg"
+            mr="lg"
+            ml="lg"
+          />
+        </Header>
+      }
+      navbar={
+        <Navbar
+          // className={classes.navbar}
+          width={{ base: '100%' }}
+          hidden={!opened}
+        >
+          <Anchor>Home</Anchor>
+          <Anchor>Features</Anchor>
+          <Anchor>Pricing</Anchor>
+        </Navbar>
+      }
+    >
+      App
+    </AppShell>
   );
 }
 
