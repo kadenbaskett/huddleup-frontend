3ab0978932c7c7217b5f350802d087b9bb419ee5
jakewhiteyo <jakewhiteyo@gmail.com>
2022-11-09 15:04:49 -0700

    added font, colors, partial nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        varsity: ['Varsity Team'],
+      },
+      colors: {
+        green: '#1A9345',
+        orange: '#FF6B00',
+        darkBlue: '#0C0729',
+      },
+    },
   },
   plugins: [],
-}
+};
