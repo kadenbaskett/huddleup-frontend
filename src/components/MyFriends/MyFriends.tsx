bd1d6fc4a05bccbed901690042c6be4a06d61bba
jakewhiteyo <jakewhiteyo@gmail.com>
2022-11-14 13:38:10 -0700

    fixed myfriends component

--- src/components/MyFriends/MyFriends.tsx
+++ src/components/MyFriends/MyFriends.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './MyFriends.module.css';
 
+const names = ['jake', 'joe', 'justin', 'kaden'];
+
 export default function MyFriends() {
+  const renderRows = () => {
+    return names.map((name) => renderRow(name));
+  };
+
+  const renderRow = (name: String) => {
+    return <div className='flex justify-center p-3 text-2xl '>{name}</div>;
+  };
+
   return (
     <div className='grid grid-cols-1 bg-white rounded-xl'>
       <div className='flex huddleFont justify-left p-4 text-3xl bg-darkBlue text-white rounded-t-xl'>
         My Friends
       </div>
-      <div className='flex justify-center p-3 text-2xl '>Joseph Cockman</div>
-      <div className='flex justify-center p-3 text-2xl '>Jake Pussyite</div>
-      <div className='flex justify-center p-3 text-2xl '>Kaden Bobskett</div>
-      <div className='flex justify-center p-3 text-2xl '>Justin Cuntez </div>
+      {renderRows()}
     </div>
   );
 }
