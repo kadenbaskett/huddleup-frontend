61b287a00ce393c2893857abf493dce05c0238d1
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2022-11-14 14:19:31 -0700

    Fix font

--- src/components/MyTeams/MyTeams.tsx
+++ src/components/MyTeams/MyTeams.tsx
@@ -4,7 +4,7 @@ import './MyTeams.module.css';
 export default function MyTeams() {
   return (
     <div className='grid grid-cols-1 bg-white rounded-xl'>
-      <div className='flex huddleFont justify-left p-4 text-3xl bg-darkBlue text-white rounded-t-xl'>
+      <div className='flex font-varsity justify-left p-4 text-3xl bg-darkBlue text-white rounded-t-xl'>
         My Teams
       </div>
       <div className='flex justify-center p-3 text-2xl '>this sucks</div>
