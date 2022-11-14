42757992fde9a6b4e4970ecc935f99c932f8b9f1
jakewhiteyo <jakewhiteyo@gmail.com>
2022-11-14 12:12:23 -0700

    fixed font

--- components/MyNews/MyNews.tsx
+++ components/MyNews/MyNews.tsx
@@ -4,7 +4,7 @@ import './MyNews.module.css'
 export default function MyNews() {
   return (
     <div className='grid grid-cols-1 bg-white rounded-xl'>
-      <div className='flex font-varsity justify-left p-4 text-3xl bg-darkBlue text-white rounded-t-xl'>
+      <div className='flex huddleFont justify-left p-4 text-3xl bg-darkBlue text-white rounded-t-xl'>
         News
       </div>
     </div>
