aaa7ffb46fb889a026f499f27314bc73348d3999
jakewhiteyo <jakewhiteyo@gmail.com>
2022-11-16 12:13:19 -0700

    created leagues page

---
+++ src/components/LeagueCard/LeagueCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LeagueCard() {
+  return (
+    <div className='grid grid-cols-5 bg-white rounded-xl h-80'>
+      <div className='grid-col-1'> League Card</div>
+    </div>
+  );
+}
+
+export default LeagueCard;
--- src/pages/leagues/index.tsx
+++ src/pages/leagues/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import LeagueCard from '../../components/LeagueCard/LeagueCard';
 
 function leagues() {
-  return <div>leagues</div>;
+  return (
+    <div className='grid grid-cols-10 bg-slate-300 p-10 min-h-screen gap-3'>
+      <div className='col-span-4'>
+        <div className='grid grid-cols-2 gap-3'>
+          <button className='py-7 px-20 hover:bg-transparent hover:text-orange text-xl font-bold border border-orange rounded bg-orange text-white transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>
+            Join a League
+          </button>
+          <button className='py-7 px-20 hover:bg-transparent hover:text-green text-xl font-bold border border-green rounded bg-green text-white transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>
+            Create a League
+          </button>
+        </div>
+      </div>
+      <div className='col-span-10'>
+        <LeagueCard />
+      </div>
+    </div>
+  );
 }
 
 export default leagues;
