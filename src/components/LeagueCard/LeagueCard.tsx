bff5ca18c9ed87db69fd08e5c200289e1448b1b8
Justin Perez <u1248103@umail.utah.edu>
2022-11-30 14:41:52 -0700

    finished the create a league page

--- src/components/LeagueCard/LeagueCard.tsx
+++ src/components/LeagueCard/LeagueCard.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import Image from 'next/image';
 import NFL from '../../public/assets/NFL.png';
 
-export interface leagueCardProps {
+export interface leagueProps {
   name: string;
   subText: string;
+  id: string;
 }
 
-export function LeagueCard(leagueCard: leagueCardProps) {
+export function LeagueCard(league: leagueProps) {
   return (
-    <a href=''>
+    <a href={'/leagues/' + league.id + '/home'}>
       <div className='grid grid-cols-5 bg-white rounded-xl h-80 border hover:border-orange border-white'>
         <div
           className='grid col-span-1 items-center'
           style={{width: 'auto', height: '17rem', position: 'relative'}}
         >
-          <Image src={NFL} alt={leagueCard.name + '-image'} layout='fill' objectFit='contain' />
+          <Image src={NFL} alt={league.name + '-image'} layout='fill' objectFit='contain' />
         </div>
         <div className='grid col-span-4  items-center'>
           <div className='grid grid-rows-2'>
-            <div className='text-8xl font-varsity text-darkBlue'>{leagueCard.name}</div>
-            <div className='text-3xl font-OpenSans text-orange'>{leagueCard.subText}</div>
+            <div className='text-8xl font-varsity text-darkBlue'>{league.name}</div>
+            <div className='text-3xl font-OpenSans text-orange'>{league.subText}</div>
           </div>
         </div>
       </div>
--- src/pages/leagues/create/index.tsx
+++ src/pages/leagues/create/index.tsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
-import {Slider, Textarea, TextInput} from '@mantine/core';
-import Link from 'next/link';
-
+import {Button, Group, RangeSlider, Slider, Textarea, TextInput} from '@mantine/core';
 interface teamSlider {
   value: number;
   label: string;
@@ -33,9 +31,21 @@ const numPlayers: numPlayersSlider[] = [
 
 export default function index() {
   const [teams, setTeamValue] = useState(10);
-  const [players, setPlayersValue] = useState(2);
+  const [minPlayerRange, setMinPlayerRange] = useState(2);
+  const [maxPlayerRange, setMaxPlayerRange] = useState(4);
+
+  function setRange(value: number[]) {
+    setMinPlayerRange(value[0]);
+    setMaxPlayerRange(value[1]);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('event', event.target);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='grid grid-rows-10 bg-lightGrey p-10 min-h-screen gap-10'>
         <div>
           <label className='font-varsity text-6xl'>Create Your League</label>
@@ -45,6 +55,7 @@ export default function index() {
           <TextInput
             placeholder='Sample League Name'
             size='xl'
+            required
             styles={() => ({
               input: {
                 fontFamily: 'Varsity Team',
@@ -73,7 +84,6 @@ export default function index() {
               track: {
                 backgroundColor: '#ff6b00',
               },
-              markLabel: {display: 'none'},
             })}
             color='orange'
           />
@@ -81,18 +91,21 @@ export default function index() {
 
         <div>
           <label className='font-OpenSans font-bold text-2xl'>
-            Number of Players per Team: {players}
+            {'Number of Players per Team:'}{' '}
+            {minPlayerRange === maxPlayerRange
+              ? `${minPlayerRange}`
+              : `${minPlayerRange} - ${maxPlayerRange}`}
           </label>
-          <Slider
+          <RangeSlider
             label={(val: number) => numPlayers.find((mark) => mark.value === val)?.label}
             labelTransition='skew-down'
             labelTransitionDuration={150}
             labelTransitionTimingFunction='ease'
             min={2}
             max={6}
-            value={players}
-            defaultValue={2}
-            onChange={setPlayersValue}
+            value={[minPlayerRange, maxPlayerRange]}
+            onChange={setRange}
+            minRange={0}
             step={1}
             marks={numPlayers}
             color='orange'
@@ -103,27 +116,29 @@ export default function index() {
           <label className='font-OpenSans font-bold text-2xl'>Scoring:</label>
           <div className='grid grid-cols-2 gap-4'>
             <div>
-              <input
-                type='radio'
-                id='PPR'
-                name='hosting'
-                value='PPR'
-                className='hidden peer'
-                required
-              />
+              <input type='radio' id='PPR' name='hosting' value='PPR' className='hidden peer' />
               <label
                 htmlFor='PPR'
                 className='inline-flex justify-between items-center p-5 w-full text-darkBlue bg-white rounded-lg border border-white cursor-pointer hover:text-orange border-orange peer-checked:text-orange peer-checked:border-orange peer-checked:text-orange hover:text-orange hover:bg-gray-100 text-darkBlue'
               >
                 <div className='block'>
                   <div className='w-full text-lg font-OpenSans font-bold'>Points Per Reception</div>
-                  <div className='w-full font-OpenSans'>Good for small websites</div>
+                  <div className='w-full font-OpenSans'>
+                    Get extra points for catches by receivers
+                  </div>
                 </div>
               </label>
             </div>
 
             <div>
-              <input type='radio' id='NPPR' name='hosting' value='NPPR' className='hidden peer' />
+              <input
+                type='radio'
+                defaultChecked
+                id='NPPR'
+                name='hosting'
+                value='NPPR'
+                className='hidden peer'
+              />
               <label
                 htmlFor='NPPR'
                 className='inline-flex justify-between items-center p-5 w-full text-darkBlue bg-white rounded-lg border border-white cursor-pointer hover:text-orange border-orange peer-checked:text-orange peer-checked:border-orange peer-checked:text-orange hover:text-orange hover:bg-gray-100 text-darkBlue'
@@ -132,64 +147,7 @@ export default function index() {
                   <div className='w-full text-lg font-OpenSans font-bold'>
                     No Points Per Reception
                   </div>
-                  <div className='w-full'>Good for large websites</div>
-                </div>
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <label className='font-OpenSans font-bold text-2xl'>Management Style: </label>
-          <div className='grid grid-cols-3 gap-4'>
-            <div>
-              <input
-                type='radio'
-                id='voting'
-                name='hosting'
-                value='voting'
-                className='hidden peer'
-                required
-              />
-              <label
-                htmlFor='voting'
-                className='inline-flex justify-between items-center p-5 w-full text-darkBlue bg-white rounded-lg border border-white cursor-pointer hover:text-orange border-orange peer-checked:text-orange peer-checked:border-orange peer-checked:text-orange hover:text-orange hover:bg-gray-100 text-darkBlue'
-              >
-                <div className='block'>
-                  <div className='w-full text-lg font-OpenSans font-bold'>Voting</div>
-                  <div className='w-full font-OpenSans'>Good for small websites</div>
-                </div>
-              </label>
-            </div>
-
-            <div>
-              <input
-                type='radio'
-                id='rotating'
-                name='hosting'
-                value='rotating'
-                className='hidden peer'
-              />
-              <label
-                htmlFor='rotating'
-                className='inline-flex justify-between items-center p-5 w-full text-darkBlue bg-white rounded-lg border border-white cursor-pointer hover:text-orange border-orange peer-checked:text-orange peer-checked:border-orange peer-checked:text-orange hover:text-orange hover:bg-gray-100 text-darkBlue'
-              >
-                <div className='block'>
-                  <div className='w-full text-lg font-OpenSans font-bold'>Rotating</div>
-                  <div className='w-full'>Good for large websites</div>
-                </div>
-              </label>
-            </div>
-
-            <div>
-              <input type='radio' id='ffa' name='hosting' value='ffa' className='hidden peer' />
-              <label
-                htmlFor='ffa'
-                className='inline-flex justify-between items-center p-5 w-full text-darkBlue bg-white rounded-lg border border-white cursor-pointer hover:text-orange border-orange peer-checked:text-orange peer-checked:border-orange peer-checked:text-orange hover:text-orange hover:bg-gray-100 text-darkBlue'
-              >
-                <div className='block'>
-                  <div className='w-full text-lg font-OpenSans font-bold'>Free-For-All</div>
-                  <div className='w-full'>Good for large websites</div>
+                  <div className='w-full'>Don't get extra points for catches by receivers</div>
                 </div>
               </label>
             </div>
@@ -201,28 +159,22 @@ export default function index() {
           <Textarea placeholder='Add a league description' autosize minRows={2} size='xl' />
         </div>
 
-        <div className='grid grid-cols-10'>
-          <div className='grid col-start-4 col-span-3 grid-cols-2 gap-10'>
-            <div>
-              <Link href={'/leagues'}>
-                <button className='py-7 px-20 hover:bg-transparent hover:text-orange text-xl font-bold hover:border hover:border-orange rounded bg-orange text-white border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'>
-                  Cancel
-                </button>
-              </Link>
-            </div>
-
-            <div>
-              <Link href={'/leagues'}>
-                <button
-                  value='submit'
-                  className='py-7 px-20 hover:bg-transparent hover:text-green text-xl font-bold hover:border hover:border-green rounded bg-green text-white border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'
-                >
-                  Create
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
+        <Group position='center'>
+          <Button
+            className='hover:bg-transparent hover:text-orange text-xl font-bold hover:border hover:border-orange rounded bg-orange text-white border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'
+            variant='default'
+            size='xl'
+          >
+            Cancel
+          </Button>
+          <Button
+            className='hover:bg-transparent hover:text-green text-xl font-bold hover:border hover:border-green rounded bg-green text-white border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0'
+            variant='default'
+            size='xl'
+          >
+            Submit
+          </Button>
+        </Group>
       </div>
     </form>
   );
