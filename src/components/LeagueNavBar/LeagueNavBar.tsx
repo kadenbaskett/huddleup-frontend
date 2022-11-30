dac5622f386df8e612f7c35dd7223fefc808f8e1
jakewhiteyo <jakewhiteyo@gmail.com>
2022-11-30 15:10:40 -0700

    began work on the league navbar

---
+++ src/components/LeagueNavBar/LeagueNavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function LeagueNavBar() {
+  return (
+    <>
+      <div className='grid bg-slate-300 h-40 flex items-start"'>
+        <p>ayo</p>
+      </div>
+      <nav></nav>
+    </>
+  );
+}
--- src/pages/leagues/[leagueId]/home/index.tsx
+++ src/pages/leagues/[leagueId]/home/index.tsx
@@ -1,10 +1,16 @@
 import {useRouter} from 'next/router';
 import React from 'react';
+import LeagueNavBar from '../../../../components/LeagueNavBar/LeagueNavBar';
 
 function league() {
   const router = useRouter();
   const {leagueId} = router.query;
-  return <div>This will display the information of league - {leagueId}</div>;
+  return (
+    <>
+      <LeagueNavBar />
+      <div>This will display the information of league - {leagueId}</div>
+    </>
+  );
 }
 
 export default league;
