6f5d09163e6344e930f656b9fb1c8e321702072c
Justin Perez <u1248103@umail.utah.edu>
2023-01-11 18:13:48 -0700

    added the mock data to the standings. basically done at this point

--- src/pages/leagues/[leagueId]/standings/components/StandingsTable/StandingsTable.tsx
+++ src/pages/leagues/[leagueId]/standings/components/StandingsTable/StandingsTable.tsx
@@ -6,7 +6,18 @@ export interface StandingsTableProps {
   teams: Team[]; // this is the same teams that is in my overview page. Should we have a global types folder for this kind of thing?
 }
 
-export default function StandingsTable() {
+export default function StandingsTable(props: StandingsTableProps) {
+  const rows = props.teams.map((p: Team) => (
+    <tr key={p.id.toString()}>
+      <td>{p.rank.toString()}</td>
+      <td>{p.name}</td> {/* These will ideally link to the specific teams page */}
+      <td>{p.managers}</td>{' '}
+      {/* These will ideally link to the specific persons page (stretch feature) */}
+      <td>{p.wins.toString()}</td>
+      <td>{p.losses.toString()}</td>
+      <td>0</td>
+    </tr>
+  ));
   return (
     <div className='bg-white rounded-xl hover:drop-shadow-md'>
       <div className='p-4 font-varsity justify-left text-2xl bg-darkBlue text-white rounded-t-xl'>
@@ -15,6 +26,7 @@ export default function StandingsTable() {
       <Table>
         <thead>
           <tr>
+            <th>Rank</th>
             <th>Team</th>
             <th>Managers</th>
             <th>Wins</th>
@@ -22,7 +34,7 @@ export default function StandingsTable() {
             <th>Ties</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{rows}</tbody>
       </Table>
     </div>
   );
