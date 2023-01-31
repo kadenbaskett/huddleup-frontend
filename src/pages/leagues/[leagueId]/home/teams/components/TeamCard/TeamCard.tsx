9d476e9c82bdec803aaf618c6ff17075499e0ea0
Justin Perez <u1248103@umail.utah.edu>
2023-01-31 16:27:47 -0700

    started working on the teams page

---
+++ src/pages/leagues/[leagueId]/home/teams/components/TeamCard/TeamCard.tsx
@@ -0,0 +1,10 @@
+import { Team } from '@interfaces/league.interface';
+import React from 'react';
+
+export interface TeamCardProps {
+  teams: Team[];
+}
+
+export default function TeamCard(props: TeamCardProps) {
+  return <div>TeamCard</div>;
+}
--- src/pages/leagues/[leagueId]/home/teams/index.tsx
+++ src/pages/leagues/[leagueId]/home/teams/index.tsx
@@ -1,25 +1,18 @@
+import { HuddleUpLoader } from '@components/HuddleUpLoader/HuddleUpLoader';
 import LeagueHomeNavigation from '@components/LeagueHomeNavigation/LeagueHomeNavigation';
 import LeagueNavBar from '@components/LeagueNavBar/LeagueNavBar';
 import { Table } from '@mantine/core';
-import { fetchLeagueInfoThunk } from '@store/slices/leagueSlice';
-import { AppDispatch, StoreState } from '@store/store';
+import { StoreState } from '@store/store';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function index() {
   const router = useRouter();
   const { leagueId } = router.query;
 
-  const dispatch = useDispatch<AppDispatch>();
-  const leagueInfoFetchStatus = useSelector((state: StoreState) => state.league.leagueFetchStatus);
+  const leagueInfoFetchStatus = useSelector((state: StoreState) => state.league.status);
   const league = useSelector((state: StoreState) => state.league.league);
-
-  useEffect(() => {
-    if (leagueInfoFetchStatus === 'idle' && leagueId) {
-      dispatch(fetchLeagueInfoThunk(Number(leagueId)));
-    }
-  }, [leagueInfoFetchStatus, dispatch, leagueId]);
 
   const teams = league ? league.teams : [];
 
@@ -45,23 +38,28 @@ function index() {
         leagueId={Number(leagueId)}
         page='home'
       />
-      <LeagueHomeNavigation
-        leagueId={Number(leagueId)}
-        leagueName={league ? league.name : ' '}
-        leagueDescription={'This is an example league description'}
-        page='teams'
-      />
-      <div>This is the teams page for a league</div>
-      <Table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Managers</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
+      {leagueInfoFetchStatus !== 'succeeded' && <HuddleUpLoader />}
+      {leagueInfoFetchStatus === 'succeeded' && (
+        <>
+          <LeagueHomeNavigation
+            leagueId={Number(leagueId)}
+            leagueName={league ? league.name : ' '}
+            leagueDescription={'This is an example league description'}
+            page='teams'
+          />
+          <div>This is the teams page for a league</div>
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Managers</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </Table>
+        </>
+      )}
     </div>
   );
 }
