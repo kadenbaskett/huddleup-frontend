04822777e1173bc432c06106bd31ad463b9b2572
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-02-01 20:13:49 -0700

    added dropdown

--- src/pages/home/components/MyNews/MyNews.tsx
+++ src/pages/home/components/MyNews/MyNews.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Image from 'next/image';
 import { News } from '@interfaces/news.interface';
 // import CNN from '../../../../public/assets/CNNPhoto.png';
-import Link from 'next/link';
 
 function capitalizeFirstLetter(str) {
   const upperFirstLetter: string = str.charAt(0).toUpperCase();
@@ -11,8 +10,10 @@ function capitalizeFirstLetter(str) {
 }
 
 const renderRow = (news: News) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className='grid grid-rows-3 p-4 pb-2 border-darkBlue border-4 border-t-0'>
+    <div className='p-4 pb-2 border-darkBlue border-4 border-t-0'>
       {/* <Image
         className='justify-right col-span-1 align-middle'
         src={CNN}
@@ -20,13 +21,33 @@ const renderRow = (news: News) => {
         width={92}
         height={92}
       /> */}
-      <Link
+      <a
+        target='_blank'
         href={news.source_url}
-        className='underline text-blue-600 hover:text-blue-700 justify-left text-2xl break-words align-middle'
+        className='underline text-blue-600 hover:text-blue-800 justify-left text-2xl break-words align-middle'
+        rel='noreferrer'
       >
         {news.title}
-      </Link>
-      <p>{news.content.slice(0, 200)}...</p>
+      </a>
+      {isExpanded && <p className='mt-3'>{news.content} </p>}
+      {!isExpanded && <p className='mt-3'>{news.content.slice(0, 200)}... </p>}
+      {!isExpanded && (
+        <div className='text-sm text-right'>
+          <button className='text-blue-500 hover:text-blue-700' onClick={() => setIsExpanded(true)}>
+            Show more
+          </button>
+        </div>
+      )}
+      {isExpanded && (
+        <div className='text-sm text-right'>
+          <button
+            className='text-blue-500 hover:text-blue-700'
+            onClick={() => setIsExpanded(false)}
+          >
+            Show less
+          </button>
+        </div>
+      )}
       <div className='flex items-center text-sm text-gray-700'>
         <div>Time posted: {capitalizeFirstLetter(news.time_posted)}</div>
         <div className='ml-4'>Source: {news.source}</div>
