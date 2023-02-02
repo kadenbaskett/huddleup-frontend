8a8918a11cd8446ad4ef8ec1d315529a0984b9ea
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-02-01 19:35:00 -0700

    done except extra features

--- src/pages/home/components/MyNews/MyNews.tsx
+++ src/pages/home/components/MyNews/MyNews.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { News } from '@interfaces/news.interface';
-import CNN from '../../../../public/assets/CNNPhoto.png';
+// import CNN from '../../../../public/assets/CNNPhoto.png';
+import Link from 'next/link';
+
+function capitalizeFirstLetter(str) {
+  const upperFirstLetter: string = str.charAt(0).toUpperCase();
+  const remainingString: string = str.slice(1);
+  return upperFirstLetter + remainingString;
+}
 
 const renderRow = (news: News) => {
   return (
-    <div className='grid grid-cols-8 p-3'>
-      <Image
+    <div className='grid grid-rows-3 p-4 pb-2 border-darkBlue border-4 border-t-0'>
+      {/* <Image
         className='justify-right col-span-1 align-middle'
         src={CNN}
         alt='some news photo pic'
         width={92}
         height={92}
-      />
-      <p className='justify-left col-span-7 text-2xl break-words align-middle'>{news.title}</p>
+      /> */}
+      <Link
+        href={news.source_url}
+        className='underline text-blue-600 hover:text-blue-700 justify-left text-2xl break-words align-middle'
+      >
+        {news.title}
+      </Link>
+      <p>{news.content.slice(0, 200)}...</p>
+      <div className='flex items-center text-sm text-gray-700'>
+        <div>Time posted: {capitalizeFirstLetter(news.time_posted)}</div>
+        <div className='ml-4'>Source: {news.source}</div>
+      </div>
     </div>
   );
 };
