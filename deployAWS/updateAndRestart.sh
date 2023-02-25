ec05b37a14533e4917f2dcfe00d35f344197616f
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-02-25 15:44:39 -0700

    add pipeline

---
+++ deployAWS/updateAndRestart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# any future command that fails will exit the script
+set -e
+
+# stop, update, and restart service 
+ls
+cd /huddleup/webapp
+pm2 delete webapp
+git pull
+npm i
+npm run build
+pm2 start npm --name "webapp" -- start
