582705ecdb08aaa0a5a83558cf3e45f38ba2aca1
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-02-25 15:53:04 -0700

    fix pipeline

--- deployAWS/updateAndRestart.sh
+++ deployAWS/updateAndRestart.sh
@@ -3,6 +3,9 @@
 # any future command that fails will exit the script
 set -e
 
+# Update  path inorder to use npm and pm2 commands
+export PATH="$PATH:/usr/local/bin"
+
 # stop, update, and restart service 
 ls
 cd huddleup
