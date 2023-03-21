9926ea49b8c3c5fafe9e10accef3b5d95dde58b6
Justin Perez <u1248103@umail.utah.edu>
2023-03-21 17:33:38 -0600

    fixed a bug on the player pop up on the matchups page. The player pop up would show add for someone who is on your team

--- src/interfaces/league.interface.ts
+++ src/interfaces/league.interface.ts
@@ -12,11 +12,21 @@ export interface Settings {
   num_teams: number;
   max_players: number;
   min_players: number;
-  scoring_settings: Scoring;
+  scoring_settings: ScoringSettings;
+  roster_settings: RosterSettings;
 }
 
-export interface Scoring {
+export interface ScoringSettings {
   points_per_reception: number;
+}
+
+export interface RosterSettings {
+  num_qb: number;
+  num_rb: number;
+  num_wr: number;
+  num_te: number;
+  num_flex: number;
+  roster_size_limit: number;
 }
 
 export interface Team {
