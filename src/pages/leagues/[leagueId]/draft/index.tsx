1bd42253abb614e434e24be53ca415d59c53ed41
Joseph Rodman <josephrodman5@gmail.com>
2023-03-22 18:29:26 -0600

    stufff for jakey boy

--- src/pages/leagues/[leagueId]/draft/index.tsx
+++ src/pages/leagues/[leagueId]/draft/index.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 import SockJS from 'sockjs-client';
 
 export default function index() {
-  const sock = new SockJS('http://localhost:7071');
+  const sock = new SockJS('http://localhost:9999/echo');
 
   sock.onopen = function () {
     console.log('open');
-    sock.send('test');
-  };
 
-  sock.onmessage = function (e) {
-    console.log('message', e.data);
-    sock.close();
+    const send = {
+      message: 'my message',
+      username: 'my username',
+    };
+
+    sendMessage(send);
   };
 
   sock.onclose = function () {
     console.log('close');
   };
+
+  sock.onmessage = function (e) {
+    const content = JSON.parse(e.data);
+    console.log(content);
+  };
+
+  function sendMessage(message) {
+    sock.send(JSON.stringify(message));
+  }
 
   return <div>index</div>;
 }
