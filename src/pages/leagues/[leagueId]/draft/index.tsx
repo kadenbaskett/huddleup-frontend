2b443e95a2221280a338b2a448ca304b4c0501a5
Justin Perez <u1248103@umail.utah.edu>
2023-03-24 12:22:55 -0600

    started working on the front end for the draft page

--- src/pages/leagues/[leagueId]/draft/index.tsx
+++ src/pages/leagues/[leagueId]/draft/index.tsx
@@ -3,10 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { draftActions } from '@store/slices/draftSlice';
 import { StoreState } from '@store/store';
 import DraftPlayerTable from '@components/DraftPlayerTable/DraftPlayerTable';
+import { HuddleUpLoader } from '@components/HuddleUpLoader/HuddleUpLoader';
+import { Grid, NativeSelect, SegmentedControl, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 export default function index() {
   const dispatch = useDispatch();
-
+  const leagueInfoFetchStatus: String = useSelector((state: StoreState) => state.league.status);
   const websocketConnected = useSelector((state: StoreState) => state.draft.isConnected);
   const websocketTryingToConnect = useSelector((state: StoreState) => state.draft.isConnected);
 
@@ -22,9 +25,70 @@ export default function index() {
     }
   }, []);
 
+  // Player filtering
+  const form = useForm({
+    initialValues: {
+      player: '',
+      position: 'All',
+      availability: 'Available',
+    },
+  });
+
   return (
     <>
-      <DraftPlayerTable />
+      {leagueInfoFetchStatus !== 'succeeded' && <HuddleUpLoader />}
+      {leagueInfoFetchStatus === 'succeeded' && (
+        <>
+          <div className='bg-lightGrey min-h-screen'>
+            <Grid>
+              <Grid.Col span={10} offset={1}>
+                <Grid.Col>
+                  <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <div className='bg-white rounded-xl hover:drop-shadow-md'>
+                      <div className='p-4 font-varsity justify-left text-2xl bg-darkBlue text-white rounded-t-xl'>
+                        Filters
+                      </div>
+                      <div className='pr-4 pl-4 pt-2'>
+                        <div className='text-md font-varsity text-darkBlue'>Position:</div>
+                        <SegmentedControl
+                          color='orange'
+                          fullWidth
+                          transitionDuration={400}
+                          transitionTimingFunction='linear'
+                          data={[
+                            { label: 'All', value: 'All' },
+                            { label: 'QB', value: 'QB' },
+                            { label: 'RB', value: 'RB' },
+                            { label: 'WR', value: 'WR' },
+                            { label: 'TE', value: 'TE' },
+                            { label: 'FLEX', value: 'FLEX' },
+                          ]}
+                          {...form.getInputProps('position')}
+                        />
+                      </div>
+                      <div className='pr-4 pl-4 pt-2'>
+                        <div className='text-md font-varsity text-darkBlue'>Player Name:</div>
+                        <TextInput
+                          placeholder='Justin Jefferson'
+                          {...form.getInputProps('player')}
+                        />
+                      </div>
+                      <div className='pr-4 pl-4 pt-2 pb-4'>
+                        <div className='text-md font-varsity text-darkBlue'>Availability:</div>
+                        <NativeSelect
+                          data={['All', 'Available', 'Waivers', 'Free Agents', 'On Rosters']}
+                          {...form.getInputProps('availability')}
+                        />
+                      </div>
+                    </div>
+                  </form>
+                  <DraftPlayerTable />
+                </Grid.Col>
+              </Grid.Col>
+            </Grid>
+          </div>
+        </>
+      )}
     </>
   );
 }
