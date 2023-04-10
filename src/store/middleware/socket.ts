7da23c1a8c9a10ca56c7c1313d80b1172fcdd404
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-04-09 23:36:35 -0600

    pass to ec2

--- src/store/middleware/socket.ts
+++ src/store/middleware/socket.ts
@@ -109,18 +109,16 @@ const draftMiddleware: Middleware = (store) => {
       // const url = `${CONNECTION.SCHEME}://${CONNECTION.HOST}:${draftState.draftPort}${CONNECTION.SERVER_PREFIX}`;
       const url = `https://huddleupfantasy.com/draftSocket/${draftState.draftPort}${CONNECTION.SERVER_PREFIX}`;
 
-      console.log('Socket URL: ', url);
-      console.log(
-        'Dev URL: ',
-        `${CONNECTION.SCHEME}://${CONNECTION.HOST}:${draftState.draftPort}${CONNECTION.SERVER_PREFIX}`,
-      );
-
       socket = new SockJS(url);
       // console.log('Past socket: ', socket);
 
       socket.onopen = function () {
         store.dispatch(draftActions.connectionEstablished());
         requestInitialDraftState();
+      };
+
+      socket.onerror = (error) => {
+        console.error('Socket Connection error:', error);
       };
 
       socket.onmessage = function (socketMessage) {
