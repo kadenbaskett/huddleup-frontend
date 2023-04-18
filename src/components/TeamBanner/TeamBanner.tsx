ec886ee14d9fba92439de1568e74404e459e456e
Justin Perez <u1248103@umail.utah.edu>
2023-04-18 13:43:55 -0600

    undid something

--- src/components/TeamBanner/TeamBanner.tsx
+++ src/components/TeamBanner/TeamBanner.tsx
@@ -1,5 +1,5 @@
 import { Grid } from '@mantine/core';
-import { createManagerString, useWindowResize } from '@services/helpers';
+import { createManagerString } from '@services/helpers';
 import { GiAmericanFootballHelmet } from 'react-icons/gi';
 
 export interface TeamBannerProps {
@@ -9,22 +9,19 @@ export interface TeamBannerProps {
 }
 
 export function TeamBanner(props: TeamBannerProps) {
-  const windowSize = useWindowResize();
-
-  windowSize[0] > 950 ||
-    (windowSize[0] === 0 && (
-      <div className='bg-white rounded-xl hover:drop-shadow-md'>
-        <Grid>
-          <Grid.Col span={2}>
-            <GiAmericanFootballHelmet size={75} color='' />
-          </Grid.Col>
-          <Grid.Col span={10}>
-            <div className='text-6xl font-varsity text-darkBlue'>{props.name}</div>
-            <div className='text-xl font-openSans text-orange'>
-              {createManagerString(props.team.managers)}
-            </div>
-          </Grid.Col>
-        </Grid>
-      </div>
-    ));
+  return (
+    <div className='bg-white rounded-xl hover:drop-shadow-md'>
+      <Grid>
+        <Grid.Col span={2}>
+          <GiAmericanFootballHelmet size={75} color='' />
+        </Grid.Col>
+        <Grid.Col span={10}>
+          <div className='text-6xl font-varsity text-darkBlue'>{props.name}</div>
+          <div className='text-xl font-openSans text-orange'>
+            {createManagerString(props.team.managers)}
+          </div>
+        </Grid.Col>
+      </Grid>
+    </div>
+  );
 }
