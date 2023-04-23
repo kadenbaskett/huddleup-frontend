ae8e729533b7866436340b0aa923fb708dcc1564
Joseph Rodman <josephrodman5@gmail.com>
2023-04-23 14:54:12 -0600

    initial burger setup

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,15 +7,18 @@ import { useWindowResize } from '@services/helpers';
 import { useState } from 'react';
 import ProfilePopup from '@components/ProfilePopup/ProfilePopup';
 import { logout } from '../../firebase/firebase';
+import { useDisclosure } from '@mantine/hooks';
+import { Burger } from '@mantine/core';
 
 export default function Navbar() {
   const user = useSelector((store: StoreState) => store.user.userInfo);
   const windowSize: number[] = useWindowResize();
   const [profilePopupOpen, setProfilePopupOpen] = useState(false);
+  const [burgerOpen, { toggle }] = useDisclosure(false);
 
-  const flexWrap = '';
+  let burgerVisible = false;
   if (windowSize[0] < 600 && windowSize[0] !== 0) {
-    // flexWrap = 'flex-wrap';
+    burgerVisible = true;
   }
 
   const onProfileClick = () => {
@@ -30,11 +33,94 @@ export default function Navbar() {
     await logout();
   };
 
-  const dontShow = false;
+  const getHomeAndLeagues = () => {
+    return user === null ? (
+      <></>
+    ) : (
+      <>
+        <li className='nav-item'>
+          <Link
+            className='px-2 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
+            href='/home'
+          >
+            Home
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            className='px-2 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
+            href='/leagues'
+          >
+            Leagues
+          </Link>
+        </li>
+      </>
+    );
+  };
+
+  const getLoginOrProfile = () => {
+    return (
+      <li className='nav-item'>
+        {user == null ? (
+          <Link
+            className='bg-orange rounded-lg px-2 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
+            href='/login'
+          >
+            Login
+          </Link>
+        ) : (
+          <Link
+            href='#'
+            className='text-orange px-2 py-2 flex items-center  hover:opacity-75'
+            onClick={() => onProfileClick()}
+          >
+            <FaRegUser size='1.7rem' />
+          </Link>
+        )}
+      </li>
+    );
+  };
+
+  const getBurger = () => {
+    return (
+      <li className='nav-item py-2'>
+        <Burger
+          size='xl'
+          color='white'
+          className='bg-transparent'
+          opened={burgerOpen}
+          onClick={toggle}
+        />
+      </li>
+    );
+  };
+
+  const navItems = () => {
+    return (
+      <ul className='flex list-none'>
+        {!burgerVisible ? (
+          <>
+            {getHomeAndLeagues()}
+            <li className='nav-item'>
+              <Link
+                className='px-2 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
+                href='/about'
+              >
+                About
+              </Link>
+            </li>
+            {getLoginOrProfile()}
+          </>
+        ) : (
+          <></>
+        )}
+      </ul>
+    );
+  };
 
   return (
     <>
-      <nav className={`flex flex-wrap items-center justify-between py-3 bg-darkBlue ${flexWrap}`}>
+      <nav className={'flex flex-wrap items-center justify-between py-3 bg-darkBlue'}>
         <div className='pl-4'>
           <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
             <Link
@@ -46,65 +132,8 @@ export default function Navbar() {
           </div>
         </div>
         <div className={'flex items-center pr-4'} id='example-navbar-danger'>
-          <ul className='flex list-none'>
-            {user !== null && (
-              <>
-                <li className='nav-item'>
-                  <Link
-                    className=' py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
-                    href='/home'
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  {dontShow && (
-                    <Link
-                      className='pl-5 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
-                      href='/friends'
-                    >
-                      Friends
-                    </Link>
-                  )}
-                </li>
-
-                <li className='nav-item'>
-                  <Link
-                    className='pl-5 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
-                    href='/leagues'
-                  >
-                    Leagues
-                  </Link>
-                </li>
-              </>
-            )}
-            <li className='nav-item pr-5'>
-              <Link
-                className='pl-5 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
-                href='/about'
-              >
-                About
-              </Link>
-            </li>
-            <li className='nav-item'>
-              {user == null ? (
-                <Link
-                  className='bg-orange rounded-lg px-3 py-2 flex items-center text-2xl uppercase font-varsity leading-snug text-white hover:opacity-75'
-                  href='/login'
-                >
-                  Login
-                </Link>
-              ) : (
-                <Link
-                  href='#'
-                  className='text-orange px-3 py-2 flex items-center  hover:opacity-75'
-                  onClick={() => onProfileClick()}
-                >
-                  <FaRegUser size='1.7rem' />
-                </Link>
-              )}
-            </li>
-          </ul>
+          {navItems()}
+          {getBurger()}
           {user !== null && (
             <ProfilePopup
               opened={profilePopupOpen}
